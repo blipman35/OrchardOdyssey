993cc27335628bfbb1f3dec457018fa25cce4170
William Dravenstott <will.dravenstott@gmail.com>
2024-04-08 13:09:21 -0600

    Added all class files, structure equal to prev proj, test on background

---
+++ src/test/java/com/project/TestBackground.java
@@ -0,0 +1,14 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestBackground {
+
+    @Test
+    public void testBackground() {
+        Background background = new Background();
+        assertNotNull(background);
+    }
+
+}
--- src/test/java/com/project/TestGame.java
+++ src/test/java/com/project/TestGame.java
@@ -1,2 +1,4 @@
+package com.project;
+
 public class TestGame {
 }
