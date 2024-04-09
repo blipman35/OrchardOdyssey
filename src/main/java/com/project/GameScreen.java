332155b19fdfc571f2353d3875df133f443577b4
Jaden Snell <jasn7628@colorado.edu>
2024-04-09 15:43:01 -0600

    adding some additional logic

--- src/main/java/com/project/GameScreen.java
+++ src/main/java/com/project/GameScreen.java
@@ -12,6 +12,8 @@ public class GameScreen extends JPanel implements Runnable{
    final int screenWidth = scaledTile * maxScreenCol;
    final int screenHeight = scaledTile * maxScreenRow;
 
+   int FPS = 60;
+   KeyInput keyI = new KeyInput();
    Thread gameThread;
     public  GameScreen(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
@@ -26,16 +28,42 @@ public class GameScreen extends JPanel implements Runnable{
 
     @Override
     public void run() {
-        while(gameThread != null){
-            //Update game info
-            update();
-            //Draw screen with updated information
-            repaint();
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS:" + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
         }
     }
 
     public void update(){
-
+        if(keyI.upPressed == true){
+            //jump logic
+        }else{
+            //crouch logic
+        }
     }
     public void paintComponent(Graphics graphics){
         super.paintComponent(graphics);
