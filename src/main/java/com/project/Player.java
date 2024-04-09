e88ab63df60142e3af58e4cf8fc198104ecf3490
William Dravenstott <will.dravenstott@gmail.com>
2024-04-09 16:50:01 -0600

    Added jump component to the player entity.

--- src/main/java/com/project/Player.java
+++ src/main/java/com/project/Player.java
@@ -6,6 +6,10 @@ public class Player extends Entity{
     GameScreen gs;
     KeyInput keyI;
 
+    final int startingY = 300;
+    private int speedY = 0;
+    private boolean isJumping = false;
+
     public Player(GameScreen gs, KeyInput keyI){
         this.gs = gs;
         this.keyI = keyI;
@@ -15,13 +19,33 @@ public class Player extends Entity{
 
     public void setDefaultValues() {
         x = 100;
-        y = 100;
+        y = 300;
         speed = 4;
+    }
+    public void accelerate(double accelerationY) {
+        speedY += accelerationY;
+    }
+
+    public void move(double yDelta) {
+        y += yDelta;
+        // do collision detection here. upon collision, set speedX/speedY to zero..!
     }
 
     public void update(){
-        if(keyI.upPressed == true){
-            //jump logic
+        if (y > startingY) {
+            speedY = 0;
+            y = startingY;
+            isJumping = false;
+        }
+
+        if(speedY != 0 || y < startingY){
+            move(speedY);
+            accelerate(1);
+        }
+
+        if(keyI.upPressed && !isJumping){
+            isJumping = true;;
+            accelerate(-20);
         }else{
             //crouch logic
         }
