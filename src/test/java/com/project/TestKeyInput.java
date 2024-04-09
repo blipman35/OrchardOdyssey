3d2dc26470d726c11e3b403f7f4f826b9cff3e5a
Ben Lipman <benlipman35@gmail.com>
2024-04-09 15:23:47 -0600

    Tests for gamescreen and keyinput

---
+++ src/test/java/com/project/TestKeyInput.java
@@ -0,0 +1,28 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestKeyInput {
+
+    @Test
+    public void testKeyInput() {
+        KeyInput keyInput = new KeyInput();
+        
+    }
+
+    @Test
+    public void testKeyTyped() {
+
+    }
+
+    @Test
+    public void testKeyPressed() {
+
+    }
+
+    @Test
+    public void testKeyReleased() {
+
+    }
+}
