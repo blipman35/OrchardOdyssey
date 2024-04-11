b0a3b1d3fec6bdffd3ab76e3f06577223dfdd0dd
Ben Lipman <benlipman35@gmail.com>
2024-04-11 16:02:49 -0600

    Some tests for fruit

--- src/test/java/com/project/TestFruits.java
+++ src/test/java/com/project/TestFruits.java
@@ -3,6 +3,7 @@ package com.project;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
+import java.awt.*;
 
 public class TestFruits {
 
@@ -12,5 +13,27 @@ public class TestFruits {
     public void setUp() { fruits = new Fruits(0); }
 
     @Test
-    public void testFruits() { }
+    public void testFruits() {
+        assertNotNull(fruits);
+    }
+
+    @Test
+    public void testUpdate() {
+        assert true;
+    }
+
+    @Test
+    public void testCreate() {
+        assert true;
+    }
+
+    @Test
+    public void testHasCollidedFruit() {
+        fruits.hasCollidedFruit();
+    }
+
+    @Test
+    public void testResume() {
+        fruits.resume();
+    }
 }
