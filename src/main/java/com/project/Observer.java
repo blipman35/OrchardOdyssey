905e7a81a0217f8270042758ce1f0872875ebc94
Will Dravenstott <will.dravenstott@gmail.com>
2024-05-03 10:24:32 -0600

    Added additional comments

--- src/main/java/com/project/Observer.java
+++ src/main/java/com/project/Observer.java
@@ -16,6 +16,8 @@ public class Observer implements IObserver{
         this.messages = new ArrayList<>();
     }
 
+    // Print out the event and its description to the console
+    // If the event is in the list of events to observe, add the description to the messages list
     @Override
     public void update(EventType event, String eventDescription) {
         System.out.println("Observer " + name + " received event " + event + " with description: " + eventDescription);
@@ -24,6 +26,7 @@ public class Observer implements IObserver{
         }
     }
 
+    // Function to get the past events for logging purposes
     public String getLastEventDescription() {
         if (messages.size() == 0) {
             return "No events received";
